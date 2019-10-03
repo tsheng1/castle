5f28910137916a338559e307d32740b01b009365
tsheng <tsheng1@jhu.edu>
2019-10-03 10:03:46 -0400

    Fix signin and signup bug

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -1,4 +1,4 @@
-import {RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER} from "../actions/session_actions";
+import {RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER, RECEIVE_SESSION_ERRORS} from "../actions/session_actions";
 
 const _nullSession = {
   id: null
@@ -9,6 +9,8 @@ export default (state = _nullSession, action) => {
     case RECEIVE_CURRENT_USER:
       return { id: action.currentUser.id };
     case LOGOUT_CURRENT_USER:
+      return _nullSession;
+    case RECEIVE_SESSION_ERRORS:
       return _nullSession;
     default:
       return state;
