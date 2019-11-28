c5ac7e0faa212e25119417d24827198973d77551
Tony Sheng <tsheng1@jhu.edu>
2019-11-28 00:11:07 -0500

    Update store

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -12,6 +12,6 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const configureStore = (preloadedState = {}) =>
-  createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger));
+  createStore(rootReducer, preloadedState, applyMiddleware(...middlewares));
 
 export default configureStore;
